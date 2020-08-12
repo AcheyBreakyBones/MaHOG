73c136555606ba2b55ac4932d4d00751eac11b9b
Jay Sharma <j.sharma@digipen.edu>
2020-08-11 23:10:15 -0700

    Setup basic Application and Entry Point

---
+++ src/Engine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// For use by Engine applications
+#include <stdio.h>
+#include "Engine/Application.h"
+
+// ENTRY POINT
+#include "Engine/EntryPoint.h"
---
+++ src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <stdio.h>
+namespace Engine
+{
+  void Print()
+  {
+    printf("Welcome to Engine!\n");
+  }
+}
